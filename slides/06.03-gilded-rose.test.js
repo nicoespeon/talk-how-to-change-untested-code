e179c1a2731f4b1b3b543b0d164cb809a76ce92d
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-02-04 08:07:29 -0500

    Add Gilded Rose environment

---
+++ slides/06.03-gilded-rose.test.js
@@ -0,0 +1,9 @@
+const { Item, Shop } = require("./06.02-gilded-rose");
+
+it("should not throw", () => {
+  const gildedRose = new Shop([new Item("foo", 0, 0)]);
+
+  const items = gildedRose.updateQuality();
+
+  expect(items[0].name).toEqual("fixme");
+});
